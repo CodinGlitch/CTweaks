91109299f3a700f03ccb6b96821790561053b29a
CodinGlitch <robbymorris319@outlook.com>
2021-07-07 07:04:55 -0700

    Added fox squeak sound as separate sound event

--- src/main/java/com/codinglitch/ctweaks/util/SoundsC.java
+++ src/main/java/com/codinglitch/ctweaks/util/SoundsC.java
@@ -1,24 +1,29 @@
 
 package com.codinglitch.ctweaks.util;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.registry.Registry;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class SoundsC {
-    //public static SoundEvent offer;
+    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, ReferenceC.MODID);
 
     public SoundsC() {
     }
 
-    public static void registerSounds() {
-        //offer = registerSound("offer_item");
+    public static void registerBus() {
+        SOUNDS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    private static SoundEvent registerSound(String name) {
+    private static RegistryObject<SoundEvent> registerSound(String name) {
         SoundEvent soundevent = new SoundEvent(new ResourceLocation(ReferenceC.MODID, name));
-        soundevent.setRegistryName(name);
-        ForgeRegistries.SOUND_EVENTS.register(soundevent);
-        return soundevent;
+        return SOUNDS.register(name, () -> soundevent);
     }
+
+    public static RegistryObject<SoundEvent> fox_squeak = registerSound("fox_squeak");
 }
